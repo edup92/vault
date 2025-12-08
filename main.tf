1ded36dd02fdc844a000a21a68979e5128df9c1b
edup92 <edup92mail@gmail.com>
2025-12-08 13:04:36 +0000

    up

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "google_compute_firewall" "fw_lb" {
   priority  = 1000
   allow {
     protocol = "tcp"
-    ports    = ["443"]
+    ports    = ["80"]
   }
   source_ranges = ["35.191.0.0/16","130.211.0.0/22"]
   target_tags   = [google_compute_instance.instance_main.name]
