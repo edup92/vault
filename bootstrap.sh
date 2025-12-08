f565925b08189856c1c458976153890ee6db0369
edup92 <edup92mail@gmail.com>
2025-12-08 11:59:18 +0000

    up

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,14 @@
 
 ### CONFIG ###
 USERNAME="${1:-admin-$(date +%s)}"
-
-# Always ask for PROJECT_ID interactively
 read -p "Enter your Google Cloud PROJECT_ID: " PROJECT_ID
-
-ROLE="roles/editor"
+SA_EMAIL="$USERNAME@$PROJECT_ID.iam.gserviceaccount.com"
+ROLES=(
+  "roles/editor"
+  "roles/secretmanager.admin"
+  "roles/secretmanager.secretAccessor"
+  "roles/storage.admin"
+)
 
 ### 1. Select project
 echo "[INFO] Using project: $PROJECT_ID"
@@ -15,25 +18,17 @@ gcloud config set project "$PROJECT_ID"
 ### 2. Create Service Account
 echo "[INFO] Creating Service Account..."
 gcloud iam service-accounts create "$USERNAME"
-
-SA_EMAIL="$USERNAME@$PROJECT_ID.iam.gserviceaccount.com"
 echo "[INFO] Service Account created: $SA_EMAIL"
 
 ### 3. Assign role to the Service Account
-echo "[INFO] Assigning role $ROLE to $SA_EMAIL"
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$SA_EMAIL" \
-  --role="$ROLE" \
-  --quiet
+echo "[INFO] Assigning roles to $SA_EMAIL"
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$SA_EMAIL" \
-  --role="roles/secretmanager.admin"
-
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:$SA_EMAIL" \
-  --role="roles/secretmanager.secretAccessor"
-
+for ROLE in "${ROLES[@]}"; do
+  gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    --member="serviceAccount:$SA_EMAIL" \
+    --role="$ROLE" \
+    --quiet
+done
 
 ### 4. Generate JSON key inline
 echo "[INFO] Generating JSON key (inline output):"
