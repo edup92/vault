4f2a72532f52a95b36d2c9b54b8516e518a4960e
edup92 <edup92mail@gmail.com>
2025-12-16 10:40:10 +0000

    up

--- main1.tf
+++ main1.tf
@@ -27,29 +27,79 @@ resource "google_sql_user" "sqluser_main" {
 # Cloudrun
 
 resource "google_cloud_run_v2_service" "service_main" {
-  name     = local.service_main_name
-  project  = var.gcloud_project_id
+  name    = local.service_main_name
+  project = var.gcloud_project_id
   ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
   template {
     containers {
       image = local.service_image
-      ports { container_port = local.service_port }
-      env { name = "DOMAIN" value = "https://${var.dns_record}" }
-      env { name = "ROCKET_PORT" value = "${local.service_port}" }
-      env { name = "ROCKET_ADDRESS" value = "0.0.0.0" }
-      env { name = "SIGNUPS_ALLOWED" value = "false" }
-      env { name = "INVITATIONS_ALLOWED" value = "false" }
-      env { name = "WEBSOCKET_ENABLED" value = "true" }
-      env { name = "WEB_VAULT_ENABLED" value = "true" }
-      env { name = "ADMIN_TOKEN" value = var.admin_pass }
-      env { name = "TZ" value = "Europe/Madrid" }
-      env { name = "SMTP_HOST" value = var.smtp_host }
-      env { name = "SMTP_FROM" value = var.smtp_username }
-      env { name = "SMTP_PORT" value = var.smtp_port }
-      env { name = "SMTP_SECURITY" value = var.smtp_security }
-      env { name = "SMTP_USERNAME" value = var.smtp_username }
-      env { name = "SMTP_PASSWORD" value = var.smtp_password }
-      env { name  = "DATABASE_URL" value = "postgresql://vaultwarden:${var.db_password}@/vaultwarden?host=/cloudsql/${google_sql_database_instance.sql_main.connection_name}"}
+      ports {
+        container_port = local.service_port
+      }
+      env {
+        name  = "DOMAIN"
+        value = "https://${var.dns_record}"
+      }
+      env {
+        name  = "ROCKET_PORT"
+        value = local.service_port
+      }
+      env {
+        name  = "ROCKET_ADDRESS"
+        value = "0.0.0.0"
+      }
+      env {
+        name  = "SIGNUPS_ALLOWED"
+        value = "false"
+      }
+      env {
+        name  = "INVITATIONS_ALLOWED"
+        value = "false"
+      }
+      env {
+        name  = "WEBSOCKET_ENABLED"
+        value = "true"
+      }
+      env {
+        name  = "WEB_VAULT_ENABLED"
+        value = "true"
+      }
+      env {
+        name  = "ADMIN_TOKEN"
+        value = var.admin_pass
+      }
+      env {
+        name  = "TZ"
+        value = "Europe/Madrid"
+      }
+      env {
+        name  = "SMTP_HOST"
+        value = var.smtp_host
+      }
+      env {
+        name  = "SMTP_FROM"
+        value = var.smtp_username
+      }
+      env {
+        name  = "SMTP_PORT"
+        value = var.smtp_port
+      }
+      env {
+        name  = "SMTP_SECURITY"
+        value = var.smtp_security
+      }
+      env {
+        name  = "SMTP_USERNAME"
+        value = var.smtp_username
+      }
+      env {
+        name  = "SMTP_PASSWORD"
+        value = var.smtp_password
+      }
+      env {
+        name  = "DATABASE_URL"
+        value = "postgresql://vaultwarden:${var.db_password}@/vaultwarden?host=/cloudsql/${google_sql_database_instance.sql_main.connection_name}"
+      }
       resources {
         limits = {
           cpu    = local.service_cpu
@@ -69,17 +119,17 @@ resource "google_cloud_run_v2_service" "service_main" {
 
 # LB
 
-resource "google_compute_backend_service" "backend_main" {
-  name                  = local.backend_name
-  protocol              = "HTTP"
-  port_name             = "http"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  health_checks         = [google_compute_health_check.healthcheck_main.self_link]
-  connection_draining_timeout_sec = 10
-  backend {
-    group = google_compute_region_network_endpoint_group.neg.id
-  }
-  lifecycle {
-    ignore_changes = [iap]         # <- clave para no deshabilitarlo
-  }
-}
+#resource "google_compute_backend_service" "backend_main" {
+#  name                  = local.backend_name
+#  protocol              = "HTTP"
+#  port_name             = "http"
+#  load_balancing_scheme = "EXTERNAL_MANAGED"
+#  health_checks         = [google_compute_health_check.healthcheck_main.self_link]
+#  connection_draining_timeout_sec = 10
+#  backend {
+#    group = google_compute_region_network_endpoint_group.neg.id
+#  }
+#  lifecycle {
+#    ignore_changes = [iap]         # <- clave para no deshabilitarlo
+#  }
+#}
